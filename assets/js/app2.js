f842ee121183fa9fd50d511dae4f8a0fb72ee5b0
ktaufer <56054046+ktaufer@users.noreply.github.com>
2020-04-14 13:03:58 -0600

    ok

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,32 +1,37 @@
 // svg dimensions
 var svgWidth = 1200;
 var svgHeight = 600;
+
 // define margins
 var margin = {
   top: 20,
   right: 40,
   bottom: 80,
   left: 100};
+
 // chartGroup dimensions
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
 // Create an SVG wrapper
 var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
+
 // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("../../assets/data/data.csv").then(function(censusData) {
+d3.csv("../assets/data/data.csv").then(function(censusData) {
+  
   // parse data
   censusData.forEach(function(data) {
-      data.poverty = +data.poverty;
-    //   data.smokes = +data.smokes;
-      data.obesity = +data.obesity});
+    data.poverty = +data.poverty;
+    data.obesity = +data.obesity});
+  
   // Create scale functions
   var yLinearScale = d3.scaleLinear()
     .domain([0,d3.max(censusData, d => d.obesity)])
@@ -34,23 +39,31 @@ d3.csv("../../assets/data/data.csv").then(function(censusData) {
   var xLinearScale = d3.scaleLinear()
     .domain(d3.extent(censusData, d => d.poverty))
     .range([0, width]);
+  
   // Create axis functions
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
+  
   // append axes
   var xAxis = chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
   var yAxis = chartGroup.append("g")
     .call(leftAxis);
-  // append circles
-  /* Initialize tooltip */
-  var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {return (`${d.abbr}<br>Obesity(%): ${d.obesity}<br>Poverty(%): ${d.poverty}`)});
-
-/* Invoke the tip in the context of your visualization */
+  
+  // initialize tooltip
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .html(function(d) {return (`${d.state}<br>Obesity(%): ${d.obesity}<br>Poverty(%): ${d.poverty}`)});
+  
+  // create circles group
   var circlesGroup = chartGroup.selectAll('circle')
+  
+  // invoke tool tip
   chartGroup.call(tip)
-  circlesGroup.data(censusData).enter()
+  
+  // append circles
+    circlesGroup.data(censusData).enter()
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.obesity))
@@ -59,6 +72,7 @@ d3.csv("../../assets/data/data.csv").then(function(censusData) {
     .attr("opacity", ".75")
     .on('mouseover', tip.show)
     .on('mouseout', tip.hide);
+  
   // add text to circles
   circlesGroup.data(censusData)
     .enter()
@@ -69,19 +83,7 @@ d3.csv("../../assets/data/data.csv").then(function(censusData) {
     .attr('font-size', '10px')
     .attr('stroke', 'peach')
     .attr('class', 'stateText');
-  // // add tool tip
-  // var toolTip = d3.tip()
-  //   .attr("class", "tooltip")
-  //   .offset([80, -60])
-  //   .html(function(d) {
-  //       return (`${d.abbr}<br>Obesity(%): ${d.obesity}<br>Poverty(%): ${d.poverty}`)});
-  // // set mouseover and mouseout functions for tooltip
-  // //circlesGroup.call(toolTip);
-  // chartGroup
-  //   .on('mouseover', function(d) {
-  //       toolTip.show(d)})
-  //   .on('mouseout', function(d) {
-  //       toolTip.hide(d)});
+  
   // create axis labels
   var yLabel = chartGroup.append("text")
     .attr("transform", "rotate(-90)")
@@ -95,6 +97,6 @@ d3.csv("../../assets/data/data.csv").then(function(censusData) {
     .attr("x", 0)
     .attr("y", 20)
     .classed("axis-text", true)
-    .text("Population living in Poverty (%)");
+    .text("Population Living in Poverty (%)");
    
-})
+});
